218fe7cb0466cde0e2d16ac148e1490369e70a86
Nick <nickc32200@gmail.com>
2021-01-25 14:07:25 -0500

    Begin adding more web functionality.

---
+++ api.js
@@ -0,0 +1,6 @@
+
+
+
+const getSessions = async()=>{
+
+}
--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const socket = require('./socket');
+const api = require('./api');
 const express = require('express');
 const app = express();
 const PORT = 8090;
 
-app.get('/',async (req,res)=>{
+app.get('/createServer',async (req,res)=>{
 	console.log(req.query);
 	const {address,port} = req.query;
 	if(!address || !port) return res.send({success:false,message:'Invalid request parameters.'});
 	await socket.makeServer(address,port);
 	res.send({success:true});
+});
+
+app.get('/listSessions', async (req,res)=>{
+	
 });
 
 app.listen(PORT,()=>{
