8cc0523775b31738b192a66f15781ee872c6ad71
Nick <nickc32200@gmail.com>
2021-01-28 16:43:18 -0500

    Get ascii search for log messages working.

--- frontend/src/LogSidebar.js
+++ frontend/src/LogSidebar.js
@@ -7,16 +7,31 @@ export default class LogSidebar extends Component {
 	}
 	defaultProps = {
 		data:[],
-		click:()=>{}
+		click:()=>{},
+		query:''
+	}
+	handleChange(e){
+		this.setState({query:e.target.value});
+	}
+	checkQuery(log){
+		if(!this.state || !this.state.query) return true;
+		const ascii = atob(log.data);
+		if(ascii.includes(this.state.query))
+			return true;
+		return false;
 	}
 	render(){
 		let {logs} = this.props.data;
+		logs.forEach((log,i)=>log.index=i);
 		let base;
 		if(logs.length > 0)
 			base = logs[0].sourceAddress;
 		return (
-			<div className='logSidebar darkScrollbar'>
-				{logs.map((log,i)=>(<Log active={log.active} colored={log.sourceAddress !== base} name={log.name} click={this.props.click} data={log.data} destAddress={log.destAddress} srcAddress={log.sourceAddress} timestamp={log.timeStamp} index={i} />))}	
+			<div class="logFullSideBar">
+				<input onChange={this.handleChange.bind(this)} placeholder="Search" class="searchBar"/>
+				<div className='logSidebar darkScrollbar'>
+					{logs.filter(log=>this.checkQuery(log)).map((log)=>(<Log active={log.active} colored={log.sourceAddress !== base} name={log.name} click={this.props.click} data={log.data} destAddress={log.destAddress} srcAddress={log.sourceAddress} timestamp={log.timeStamp} index={log.index} />))}	
+				</div>
 			</div>
 		);
 	}
